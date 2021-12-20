160a472441a2db130cdb8d9d4f57d1d15ee5b2f5
alaeddine.hnana@sifast <alaeddine.hnana@sifast.com>
2021-12-20 11:25:39 +0100

    discover-native-script

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import {NativeScriptFormsModule, NativeScriptModule} from '@nativescript/angular
 import { AppComponent } from './app.component'
 import {CurrentChallengeComponent} from "~/app/challenges/current-challenge/current-challenge.component";
 import {StackComponent} from "../app/layouts/stack/stack.component";
+import {FlexboxComponent} from "../app/layouts/flexbox/flexbox.component";
 
 @NgModule({
   bootstrap: [AppComponent],
   imports: [NativeScriptModule,NativeScriptFormsModule],
-  declarations: [AppComponent, CurrentChallengeComponent, StackComponent],
+  declarations: [AppComponent, CurrentChallengeComponent, StackComponent, FlexboxComponent],
   providers: [],
   schemas: [NO_ERRORS_SCHEMA],
 })
